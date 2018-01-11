159bcb9805a4abbdd3878d0346d352324b8d2d0b
Collins njau <collinsnjau39@gmail.com>
2018-01-12 01:09:46 +0300

    I have added the categories model and updated the database by running the migration file

--- CraftShop/craft/models.py
+++ CraftShop/craft/models.py
@@ -5,3 +5,16 @@ class Artist(models.Model):
     first_name=models.CharField(max_length=30)
     last_name=models.CharField(max_length=30)
     email=models.EmailField()
+
+    #make class readable from shell
+    def __str__(self):
+        return self.first_name
+
+
+
+
+class categories(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name    
