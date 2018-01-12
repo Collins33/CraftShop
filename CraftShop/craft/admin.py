2646b0379d152e9a2632b01d6d193666f7cf59c4
Collins njau <collinsnjau39@gmail.com>
2018-01-12 19:28:23 +0300

    I have added the class that inherits from ModelAdmin to allow us customize models from the admin page

--- CraftShop/craft/admin.py
+++ CraftShop/craft/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Artist,categories,Craft
 
 # Register your models here.
+class CraftAdmin(admin.ModelAdmin):
+    filter_horizontal=('category',)
 
 admin.site.register(Artist)
 admin.site.register(categories)
-admin.site.register(Craft)
+admin.site.register(Craft,CraftAdmin)
