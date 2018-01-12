96cae69b87c0205c86d7d984390af7332c38234a
Collins njau <collinsnjau39@gmail.com>
2018-01-12 18:55:05 +0300

    I have created the admin page and added the models to the admin page

--- CraftShop/craft/admin.py
+++ CraftShop/craft/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Artist,categories,Craft
 
 # Register your models here.
+
+admin.site.register(Artist)
+admin.site.register(categories)
+admin.site.register(Craft)
