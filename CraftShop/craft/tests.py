4805573223524c31807b3686a1de40642ac904c0
Collins njau <collinsnjau39@gmail.com>
2018-01-12 18:34:32 +0300

    I have added the tests for the Artist model to test if it initializes correctly

--- CraftShop/craft/tests.py
+++ CraftShop/craft/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .models import Artist,categories,Craft
 # Create your tests here.
+#test the Artist model
+class ArtistTestClass(TestCase):
+    #this method will be run before each test
+    def setUp(self):
+        self.artist=Artist(first_name="collins",last_name="njau",email="collinsnjau39@gmail.com")
+
+
+    #test instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.artist,Artist))
