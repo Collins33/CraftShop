263391af146a3669391a7918a44930413ca384fd
Collins njau <collinsnjau39@gmail.com>
2018-01-26 23:32:48 +0300

    I have added the view function to add products to the cart

--- CraftShop/cart/views.py
+++ CraftShop/cart/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse
+import datetime as dt
+from craft.models import categories,Craft,NewsLetterSubscription
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from craft.email import sendEmail
+from django.contrib.auth.decorators import login_required,require_POST
+from .cart import Cart
+from .forms import CartAddProductForm
 # Create your views here.
+
+@require_POST#this form only allows post requests
+def cart_add(request,craft_id):
+    cart=Cart(request)#cart instance
+    craft=get_object_or_404(Craft,id=craft_id)#get craft that matches the id
+    forms=CartAddProductForm(request.POST)#validate the form
+    if form.is_valid():
+        cd=form.cleaned_data
+        cart.add(craft=craft,quantity=cd['quantity'],update_quantity=cd['update'])
+    return redirect('cart:cart_detail')
