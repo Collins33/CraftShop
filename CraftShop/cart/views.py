d68c8a8a13e136a870e32010812622240769c4b4
Collins njau <collinsnjau39@gmail.com>
2018-01-26 23:38:20 +0300

    I have added the view function to remove craft from the cart

--- CraftShop/cart/views.py
+++ CraftShop/cart/views.py
@@ -18,3 +18,10 @@ def cart_add(request,craft_id):
         cd=form.cleaned_data
         cart.add(craft=craft,quantity=cd['quantity'],update_quantity=cd['update'])
     return redirect('cart:cart_detail')
+
+
+def cart_remove(request,craft_id):
+    cart=Cart(session)
+    craft=get_object_or_404(Craft,id=craft_id)
+    cart.remove(craft)
+    return redirect('cart:cart_detail')
